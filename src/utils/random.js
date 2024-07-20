ca0228cb5710fa120106525de9d5aa7fd3c05370
Your Name <you@example.com>
2024-07-20 23:50:17 +0100

    web

--- src/utils/random.js
+++
@@ -1,7 +0,0 @@
-// src/utils/random.js
-export function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
